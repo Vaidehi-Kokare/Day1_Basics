a3464b6c42105ffe3371a42795d1f81759f3545a
Vaidehi-Kokare <vaidehikokare@gmail.com>
2023-01-06 18:25:14 +0530

    Added: Default Values of Data Types

---
+++ src/main/java/com/bridgelabz/basic/DefaultValues.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.basic;
+
+public class DefaultValues {
+    public static void main(String[] args) {
+        boolean x1 = false;
+        String x2 = null;
+        int x3 = 0;
+        char x4 = 0;
+        float x5 = 0;
+        long x6 = 0;
+        double x7 = 0;
+        System.out.println("Default values are................");
+        System.out.println("Boolean-" + x1);
+        System.out.println("String-" + x2);
+        System.out.println("Integer-" + x3);
+        System.out.println("Character-" + x4);
+        System.out.println("Float-" + x5);
+        System.out.println("Long-" + x6);
+        System.out.println("Double-" + x7);
+
+    }
+}
